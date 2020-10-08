4c363a7dba8603980dcd46ed5c8a318dd5d1ac32
sho0621techcamp <sho6217@gmail.com>
2020-10-08 20:26:51 +0900

    プロトタイプ削除後リダイレクト修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,7 +40,9 @@ class PrototypesController < ApplicationController
 
   def destroy
     prototype = Prototype.find(params[:id])
-    prototype.destroy
+    if prototype.destroy
+      redirect_to root_path
+    end
   end
 
   private
