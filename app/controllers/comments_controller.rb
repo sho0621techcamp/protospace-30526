33c7c44bc4a815c92f45ad3d32f4a23cdf859b1d
sho0621techcamp <sho6217@gmail.com>
2020-10-09 16:47:58 +0900

    コードレビュー後１回目の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   def create
-      comment = Comment.new(comment_params)
-    if comment.save
-      redirect_to prototype_path(comment.prototype)
+      @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
